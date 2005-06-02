17d09cc739aabc9a10862444cce7c32580c70cf3
Yuval Kogman <nothingmuch@woobling.org>
2005-06-02 23:22:51 +0300

    Parser
    darcs-hash:20050602202251-11c7b-a65f6eefe99ac1351763b11c3eb2dc7dc848da2a.gz

---
+++ src/Harrorth/Parser.hs
@@ -0,0 +1,51 @@
+module Harrorth.Parser where
+
+import Text.ParserCombinators.Parsec
+import Harrorth.AST
+
+forthProgram :: Parser Forth
+forthProgram = do
+	ast <- forth
+	eof
+	return ast
+
+forth :: Parser Forth
+forth = sepEndBy forthExp sep
+
+forthExp :: Parser Exp
+forthExp = newWord
+	   <|> literal
+	   <|> word
+
+newWord :: Parser Exp
+newWord = do
+	char ':'
+	maybeSep
+	name <- wordName
+	sep
+	body <- forth
+	maybeSep
+	char ';'
+	return $ NewWord name body
+
+literal :: Parser Exp
+literal = do
+	lit <- many1 digit
+	return $ Push (read lit)
+
+word :: Parser Exp
+word = do
+	name <- wordName
+	return $ Invoke name
+
+wordName :: Parser String
+wordName = do
+	name <- many1 letter
+	return name
+		
+sep :: Parser ()
+sep = skipMany1 space
+
+maybeSep:: Parser ()
+maybeSep = skipMany space
+

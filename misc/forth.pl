20c86f5a37dd446cec1d146f94f8890eac7026be
Yuval Kogman <nothingmuch@woobling.org>
2005-07-19 20:28:10 +0300

    Fix bootstrapping and if/else
    darcs-hash:20050719172810-11c7b-de8b39bf9e7f418cdf3f1e77c30f9b455774f08b.gz

--- misc/forth.pl
+++ misc/forth.pl
@@ -1,4 +1,4 @@
-package Interp;
+package Forth::Interp;
 
 use strict;
 use warnings;
@@ -6,14 +6,20 @@ use warnings;
 sub STATE () { 0 }
 sub IP () { 1 }
 sub DICT_HEAD () { 2 }
+sub HEADER_SIZE () { 5 }
 
 sub new {
 	my $pkg = shift;
 
+	# lexicals in closures soon
+	my @dstack = ();
+	my @rstack = ();
+	my @heap = ();
+
 	my $self = bless {
 		heap	=> [
 			0,
-			undef,
+			0,
 			0,
 		],
 		rstack	=> [],
@@ -21,37 +27,9 @@ sub new {
 	}, $pkg;
 
 	my %regPrims = (
-		"GET-LINE"	=> sub { $self->{buffer} = <> },
-		"GET-WORD"	=> sub {
-			(my($word), $_) = split(/\s+/, $_, 2) for $self->{buffer};
-			my @word = split //, $word;
-			push @{$self->{dstack}}, scalar(@{$self->{heap}}), scalar(@word);
-			push @{$self->{heap}}, @word;
-		},
-		"HERE"		=> sub { push @{$self->{dstack}}, scalar @{$self->{heap}} },
-		"ALLOT"		=> sub { push @{$self->{heap}}, undef },
-		"CELL"		=> sub { push @{$self->{dstack}}, 1 },
-		"STATE"		=> sub { push @{$self->{dstack}}, STATE },
-		"IP"		=> sub { push @{$self->{dstack}}, IP },
-		"DICT-HEAD"	=> sub { push @{$self->{dhstack}}, 2 },
-		"!"			=> sub {
-			my $address = pop @{$self->{dstack}};
-			my $value = pop @{$self->{dstack}};
-			$self->{heap}[$address] = $value;
-		},
-		"@"			=> sub { push @{$self->{dstack}}, $self->{heap}[pop @{$self->{dstack}}] },
-		"THROW"		=> sub { @{$self->{rstack}} = (); $self->{heap}[IP] = 0 },
-		"PUSH"		=> sub { push @{$self->{dstack}}, $self->{heap}[$self->{heap}[IP]++] },
-		"SWAP"		=> sub { push @{$self->{dstack}}, pop @{$self->{dstack}}, pop @{$self->{dstack}} },
-		"PICK"		=> sub { my $howmany = pop @{$self->{dstack}}; push @{$self->{dstack}}, ${$self->{dstack}}[-$howmany] },
-		"DROP"		=> sub { pop @{$self->{dstack}} },
-		"."			=> sub { print pop(@{$self->{dstack}}) . "\n" },
-		".S"		=> sub { print "[ @{$self->{dstack}} ]\n" },
-		"BSR"		=> sub {
-			push @{$self->{rstack}}, 1 + $self->{heap}[IP];
-			$self->{heap}[IP] = $self->{heap}[ $self->{heap}[IP] ];
-		},
-		"EXIT"		=> sub {
+		"BYE"		=> sub { exit },
+		"RET"		=> sub {
+			#warn "return stack is @{$self->{rstack}}, returning";
 			if (@{$self->{rstack}}) {
 				$self->{heap}[IP] = pop @{$self->{rstack}};
 			} else {
@@ -59,61 +37,201 @@ sub new {
 				last EXEC;
 			}
 		},
+		"GET-LINE"	=> sub { $self->{buffer} = <> },
+		"GET-WORD"	=> sub {
+			(my($word), $self->{buffer}) = split(/\s+/, $self->{buffer}, 2);
+			my @word = split //, $word;
+			push @{$self->{dstack}}, scalar(@{$self->{heap}}), scalar(@word);
+			push @{$self->{heap}}, @word;
+		},
+		"HEADER"	=> sub {
+			my $length = pop @{$self->{dstack}};
+			my $offset = pop @{$self->{dstack}};
+			my $addr = scalar @{$self->{heap}};
+			push @{$self->{heap}}, (
+				$self->{heap}[DICT_HEAD],
+				0, #immediate
+				0, #compile only
+				$offset,
+				$length,
+			);
+			$self->{heap}[DICT_HEAD] = $addr;
+		},
+		"WORDS" => sub {
+			my $all_words;
+
+		   	$all_words = sub {
+				my $entry = shift || return ();
+
+				#warn "reading entry at $entry... $self->{heap}[$entry .. $entry+10]";
+				
+				my $next = $self->{heap}[$entry];
+				my $offset = $self->{heap}[$entry + 3];
+				my $length = $self->{heap}[$entry + 4];
+
+				my $string = join("", @{$self->{heap}}[$offset .. (($offset + $length) - 1) ]);
+
+				return $string, &{ $all_words }($next);
+			};
+
+			my %seen;
+			my @words = grep { !$seen{$_}++ } &{$all_words}($self->{heap}[DICT_HEAD]);
+
+			local $\ = "\n";
+			local $, = ", ";
+			print @words;
+		},
+		"SEARCH-WORDLIST"	=> sub {
+			my $string = shift; # can also be used internally
+			my $push;
+			
+			unless (defined $string){
+				$push = 1;
+				my $length = pop @{$self->{dstack}};
+				my $offset = pop @{$self->{dstack}};
+				$string = join("", @{$self->{heap}}[$offset .. (($offset + $length) - 1) ]);
+			}
+
+			my $dict = $self->{heap}[DICT_HEAD];
+			while ($dict != 0){
+				my $str_ent = $dict + 3;
+
+				my ($offset, $length) = @{$self->{heap}}[$str_ent, $str_ent + 1];
+				my $entStr = join("", @{$self->{heap}}[$offset .. (($offset + $length) - 1) ]);
+				
+				last if $entStr eq $string; # found
+				
+				$dict = $self->{heap}[$dict];
+			}
+
+			$push ? (push @{$self->{dstack}}, $dict) : $dict; # if it was found it's != 0
+		},
+		"HERE"		=> sub { push @{$self->{dstack}}, scalar @{$self->{heap}} },
+		"ALLOT"		=> sub { push @{$self->{heap}}, (undef) x (pop @{$self->{dstack}}) },
+		"CELL"		=> sub { push @{$self->{dstack}}, 1 },
+		"STATE"		=> sub { push @{$self->{dstack}}, STATE },
+		"IP"		=> sub { push @{$self->{dstack}}, IP },
+		"DICT-HEAD"	=> sub { push @{$self->{dstack}}, 2 },
+		"!"			=> sub {
+			my $address = pop @{$self->{dstack}};
+			my $value = pop @{$self->{dstack}};
+			#warn "writing $value into address $address (state = " . STATE . ")";
+			$self->{heap}[$address] = $value;
+		},
+		"@"			=> sub { push @{$self->{dstack}}, $self->{heap}[pop @{$self->{dstack}}] },
+		"THROW"		=> sub { @{$self->{rstack}} = (); $self->{heap}[IP] = 0 },
+		"PUSH"		=> sub { push @{$self->{dstack}}, $self->{heap}[$self->{heap}[IP]++] },
+		"PICK"		=> sub { my $howmany = pop @{$self->{dstack}}; push @{$self->{dstack}}, ${$self->{dstack}}[-$howmany] },
+		"ROLL"		=> sub { my $howmany = pop @{$self->{dstack}}; push @{$self->{dstack}}, splice(@{$self->{dstack}}, 0-($howmany+1), 1) },
+		"DROP"		=> sub { pop @{$self->{dstack}} },
+		"."			=> sub { print pop(@{$self->{dstack}}) . "\n" },
+		".S"		=> sub { print "[ @{$self->{dstack}} ]\n" },
+		"BSR"		=> sub {
+			push @{$self->{rstack}}, 1 + $self->{heap}[IP];
+			my $addr = $self->{heap}[ $self->{heap}[IP] ];
+			#warn "jumping to addr $addr (" . $self->string_at_addr($addr) . ")";
+			
+			$self->{heap}[IP] = HEADER_SIZE + $self->{heap}[ $self->{heap}[IP] ];
+		},
 		"JNZ"		=> sub {
 			my $branch = $self->{heap}[ $self->{heap}[IP]++ ];
-			$self->{heap}[IP] = $branch if (pop @{$self->{dstack}});
+			$self->{heap}[IP] = $branch if (pop @{$self->{dstack}} == 0);
+		},
+		"JMP"		=> sub {
+			$self->{heap}[IP] = $self->{heap}[ $self->{heap}[IP]++ ];
 		},
 		"R>"		=> sub { push @{$self->{dstack}}, pop @{$self->{rstack}} },
 		"SEE"		=> sub {
 			(my($word), $self->{buffer}) = split /\s+/, $self->{buffer}, 2;
-			print ": $word ";
-			if (my $def = $self->{word_dict}{$word}){
-				while (my $cell = $self->{heap}[$def++]){
-					if ($cell == $self->{prim_dict}{EXIT}){
-						print ";\n";
+			my $lkup = $self->{prims}[$self->{prim_dict}{"SEARCH-WORDLIST"}];
+			if (my $def = &{ $lkup }($word)){
+				print ": $word ";
+
+				$def++; # dict link
+				my $immediate = $self->{heap}[$def++];
+				my $compile_only = $self->{heap}[$def++];
+				$def++;
+				$def++;
+
+				{
+					my $cell = $self->{heap}[$def++];
+					if ($cell == $self->{prim_dict}{"APPEND-TO-COMPILING"}){
+						print "APPEND-TO-COMPILING $self->{heap}[$def++] ";
+					}
+					if ($cell == $self->{prim_dict}{RET}){
+						print ";" . ($immediate ? " IMMEDIATE" : "") . "\n";
 						return;
 					} elsif($cell == $self->{prim_dict}{BSR}){
 						my $word = $self->{heap}[$def++];
-						foreach my $key (keys %{ $self->{word_dict} }){
-							if ($self->{word_dict}{$key} == $word){
-								print "$key ";
-								last;
-							}
-						}
+						my $str_ent = $word + 3;
+						my ($offset, $length) = @{$self->{heap}}[$str_ent, $str_ent + 1];
+						my $string = join("", @{$self->{heap}}[$offset .. (($offset + $length) - 1) ]);
+
+						print "<word $string> ";
 					} elsif ($cell == $self->{prim_dict}{PUSH}){
-						print "$self->{heap}[$def++] ";	
+						print "$self->{heap}[$def++] ";
 					} else {
-						print "$self->{inverted_prims}{$cell} ";
+						if ($cell == $self->{prim_dict}{JMP} or $cell == $self->{prim_dict}{JNZ}){
+							print "<$self->{prim_name_by_code}[$cell] +" . (($self->{heap}[$def++] - $def) - 1) . "> ";
+						} else {
+							print "<prim $cell $self->{prim_name_by_code}[$cell]> ";
+						}
 					}
+					redo;
 				}
 			} else {
-				print "<prim> ;\n";
+				warn "$word is not a word!";
 			}
 		},
 		"'"		=> sub {
 				(my($word), $self->{buffer}) = split /\s+/, $self->{buffer}, 2;
-				push @{$self->{dstack}}, $self->{word_dict}{$word} || $self->{prim_dict}{$word};
+				push @{$self->{dstack}}, &{ $self->{prims}[$self->{prim_dict}{"SEARCH-WORDLIST"}] }($word);
 		},
 		"COMPILE-LITERAL-AT" => sub {
 				my $address = pop @{$self->{dstack}};
 				my $value = pop @{$self->{dstack}};
 				@{$self->{heap}}[$address, $address+1] = ($self->{prim_dict}{PUSH}, $value);
 		},
-		map { $_ => eval "sub { push \@{$self->{dstack}}, (pop \@{$self->{dstack}}) $_ (pop \@{$self->{dstack}}) }" } qw(+ - * /),
+		(map { $_ => eval 'sub { push @{$self->{dstack}}, (pop @{$self->{dstack}}) '. $_ .'(pop @{$self->{dstack}}) }' } qw(+ - * /)),
+	#);
+	#my %immediatePrims = (
+		"APPEND-TO-COMPILING"	=> sub {
+				my @def = @{$self->{heap}}[$self->{rstack}[-1]++, $self->{rstack}[-1]++];
+				# @def == (code of BSR, word to jump to)
+				push @{$self->{heap}}, @def;
+		},
+		"APPEND-PRIM-TO-COMPILING" => sub {
+				$self->{rstack}[-1]++;
+				my $def = $self->{heap}[$self->{rstack}[-1]++];
+				#warn "writing prim $self->{prim_name_by_code}[$self->{heap}[$def + HEADER_SIZE]] into cell " . scalar @{$self->{heap}};
+				push @{$self->{heap}}, $self->{heap}[$def + HEADER_SIZE];
+		},
 	);
+
+	my %immediatePrims = ();
 	$_ = [ 0, $_ ] for values %regPrims;
-	my %immediatePrims = (
-		";"		=> sub { $self->{heap}[STATE] = 0 }
-	);
+	$_ = [ 1, $_ ] for values %immediatePrims;
 
-	my $hash = \%regPrims;
-	for (keys %$hash) {
-		$self->{prim_dict}{$_} = @{$self->{prims} ||= []};
-		push @{$self->{prims}}, @{ $hash->{$_} };
-		$self->{inverted_prims}{$self->{prim_dict}{$_}} = $_;
+	foreach my $hash (\%regPrims, \%immediatePrims) {
+		for (sort { ($a eq "RET") ? -1 : (($b eq "RET") ? 1 : 0) } keys %$hash) {
+			$self->{prim_dict}{$_} = @{ $self->{prims} ||= [] };
+			$self->{prim_name_by_code}[$self->{prim_dict}{$_}] = $_;
+			my ($immediate, $fun) = @{ $hash->{$_} };
+			push @{$self->{prims}}, $fun;
+
+			my @name = split //;
+			my $str = scalar @{$self->{heap}};
+			my $len = scalar @name;
+			push @{$self->{heap}}, @name;
+
+			my $cur = scalar @{$self->{heap}};
+			my $last = $self->{heap}[DICT_HEAD];
+
+			push @{$self->{heap}}, ($last, $immediate, $immediate, $str, $len, $self->{prim_dict}{$_}, $self->{prim_dict}{RET});
+
+			$self->{heap}[DICT_HEAD] = $cur;
+		}
 	}
-
-	$self->{heap}[DICT_HEAD] = scalar @{ $self->{heap} };
 
 	$self->mkprelude;
 
@@ -123,21 +241,40 @@ sub new {
 sub loop {
 	my $self = shift;
 	REDO: {
-		&{ $self->{prims}[$self->{prim_dict}{"GET-LINE"} + 1] };
+		&{ $self->{prims}[$self->{prim_dict}{"GET-LINE"}] };
 		$_ = uc for $self->{buffer};
 
 		while ($self->{buffer} =~ /\S/) {
 			(my ($word), $self->{buffer}) = split /\s+/, $self->{buffer}, 2;
-			if (my $def = $self->{word_dict}{$word}){
-				$self->{heap}[IP] = $def;
-				$self->execute;
-			} elsif (my $prim = $self->{prim_dict}{$word}) {
-				&{ $self->{prims}[$prim + 1] };
+
+			my $state = $self->{heap}[STATE];
+			my $lkup = $self->{prims}[$self->{prim_dict}{"SEARCH-WORDLIST"}];
+			
+			#warn "looping on buffer... nibbled $word, currently in state $state";
+		
+			if (my $found = &{ $lkup }($word)){
+				my $immediate = $self->{heap}[$found + 1];
+
+				if ($state && !$immediate){
+					#warn "compiling BSR $word ($found)";
+					push @{$self->{heap}}, $self->{prim_dict}{BSR}, $found;
+				} else {
+					$self->{heap}[IP] = $found + HEADER_SIZE;
+					$self->execute;
+				}
 			} elsif ($word =~ /(\d+)/){
-				push @{$self->{dstack}}, 0+$1;
+				if ($state) {
+					#warn "compiling literal $1 into stack";
+					push @{$self->{heap}}, $self->{prim_dict}{PUSH}, 0+$1;
+				} else {
+					push @{$self->{dstack}}, 0+$1;
+				}
 			} else {
 				warn "blah! $word is bullshit";
+				$self->{buffer} = "";
 			}
+
+			#warn "finished $word... arrived at state $self->{heap}[STATE]";
 		}
 		redo;
 	}
@@ -146,21 +283,16 @@ sub loop {
 sub execute {
 	my $self = shift;
 	EXEC: while ($self->{heap}[IP] < @{ $self->{heap} }){
-		my $instr = $self->{heap}[$self->{heap}[IP]++];
-		my $fun = $self->{prims}[$instr + 1]
+		my $instr = $self->{heap}[my $ip = $self->{heap}[IP]++];
+		#warn "heap = < ... @{$self->{heap}}[ ($ip - 3) .. ($ip + 2) ] ... >";
+		#warn "applying primitive $self->{prim_name_by_code}[$instr], stack=[@{$self->{dstack}}] state=$self->{heap}[STATE]";
+		my $fun = $self->{prims}[$instr]
 			or die "AAAH! invalid opcode";
 		&{ $fun };
 	}
 }
 
-my $i = Interp->new;
-
-push @{$i->{heap}}, (
-	$i->{prim_dict}{PUSH}, 0,
-	$i->{prim_dict}{JNZ}, (6 + @{$i->{heap}}),
-	$i->{prim_dict}{PUSH}, 2,
-	$i->{prim_dict}{".S"},
-);
+my $i = Forth::Interp->new;
 
 $i->loop;
 
@@ -176,16 +308,43 @@ sub mkprelude {
 	COMPILE-LITERAL-AT
 ;
 
+: ROT
+	2 ROLL
+;
+
+: SWAP
+	1 ROLL
+;
+
+: STRCMP
+	ROT
+
+;
+
+: IF
+	APPEND-PRIM-TO-COMPILING JNZ
+	HERE
+	0 ,
+; IMMEDIATE
+
+: THEN
+	HERE SWAP !
+; IMMEDIATE
+
+: ELSE
+	APPEND-PRIM-TO-COMPILING JMP
+	HERE
+	SWAP
+	0 ,
+	THEN (close off the if)
+; IMMEDIATE
+
 : EXIT
 	R> (take the pointer to caller from the return stack)
 	IP ! (write the value on the data stack into the instruction pointer)
 ;
 : EXECUTE IP ! ; (EXECUTE is sort of like go to this continuation)
-: JMP
-	R> DROP (throw away our caller)
-	EXECUTE (go to the the code on the stack)
-;
-: ; [ ' EXIT ] LITERAL COMPILE STATE OFF ; IMMEDIATE
+: ; APPEND-PRIM-TO-COMPILING RET STATE OFF ; IMMEDIATE
 : :
 	GET-WORD
 	HEADER
@@ -203,35 +362,37 @@ sub mkprelude {
 	REVEAL
 	COMPILE-VAR
 ;
+: VARIABLE
+	CREATE
+	0 ,
+;
 : LITERAL-SIZE CELL 2 * ;
 : LITERAL COMPILE-LITERAL ; IMMEDIATE
 : , HERE CELL ALLOT ! ;
-: CALL-SIZE CELL ;
+: OP-SIZE CELL ;
 : COMPILE , ; ( a call to some code is just the intruction pointer to use )
 : COMPILE-VAR
-	HERE
-	LITERAL-SIZE +
-	CALL-SIZE +
-	COMPILE-LITERAL (compile the literal for `here` after the compiled exit)
-	[ ' EXIT ] LITERAL COMPILE (COMPILE the a call to EXIT)
+	HERE LITERAL-SIZE + OP-SIZE +
+	COMPILE-LITERAL
+	APPEND-PRIM-TO-COMPILING RET
 ;
 : DOES>
 	HERE
-	DUP CALL-SIZE -
+	DUP OP-SIZE -
 	COMPILE-LITERAL-AT (overwrite the calll to 'exit' as created by 'COMPILE-VAR')
-	[ ' DOES' ] LITERAL COMPILE
-	[ ' EXIT ] LITERAL COMPILE (don't execute code appearing after DOES>)
+	APPEND-TO-COMPILING DOES'
+	APPEND-PRIM-TO-COMPILING RET (don't execute code appearing after DOES>)
 ; IMMEDIATE
 : DOES'
 	( in the word being written to, compile a literal containing )
 	LAST-BODY COMPILE-LITERAL
-	[ ' JMP ] LITERAL COMPILE
+	APPEND-PRIM-TO-COMPILING JMP
 ;
 : LAST-BODY DICT-HEAD @ HEADER-SIZE + ;
 : OPEN-DICT-ENTRY
-	DICT-HEAD DUP
-	@ , (point the current entry to the old dict head)
-	HERE OVER ! (set dictionary head to 'here')
+	DICT-HEAD
+	DUP @ , (point the current entry to the old dict head)
+	HERE SWAP ! (set dictionary head to 'here')
 ;
 : DUP 0 PICK ;
 : OVER 1 PICK ;
@@ -266,13 +427,19 @@ sub mkprelude {
 : ? @ . ;
 PRELUDE
 
-	my %bootstrap_words;
+	my (%bootstrap_words, %immediate);
 	
-	foreach my $def ($prelude =~ /:\s+(\S+.*?)\s+;\s+(IMMEDIATE)?/sg) {
+	foreach my $def ($prelude =~ /:\s+(\S+.*?)(?=:|$)/sg) {
 		$def || next;
 		$def =~ s/\(.*?\)//gs;
 		my ($name, @def) = split /\s+/, $def;
+		my $immediate = 0;
+		while ($def[$#def] ne ";"){
+			$immediate ||= (pop @def eq "IMMEDIATE");
+		}
+		pop @def;
 		$bootstrap_words{$name} = \@def;
+		$immediate{$name} = $immediate;
 	}
 
 	{
@@ -309,29 +476,43 @@ PRELUDE
 	my @ordered = map { $_->name } $d->schedule_all;
 
 	my $bsr = $self->{prim_dict}{BSR};
+	my $push = $self->{prim_dict}{PUSH};
+	my $exit = $self->{prim_dict}{RET};
+	my $lkup = $self->{prims}[$self->{prim_dict}{"SEARCH-WORDLIST"}];
+	my $hdr = $self->{prims}[$self->{prim_dict}{HEADER}];
+
 	foreach my $word (@ordered) {
+		#warn "bootstrapping $word";
 		my @def = @{$bootstrap_words{$word}};
-		$self->{word_dict}{$word} = scalar @{$self->{heap}};
+
+		my @word = split //, $word;
+		push @{$self->{dstack}}, (my @str = (scalar @{$self->{heap}}), (scalar @word));
+		push @{$self->{heap}}, @word;
+
+		my $ent = @{$self->{heap}};
+		
+		&{ $hdr }();
+
+		@{$self->{heap}}[$ent+1, $ent+2] = ($immediate{$word}) x 2; # immediate, compile only
+		
 		push @{$self->{heap}}, my @compiled = (
 			(
 				map {
-					exists $self->{prim_dict}{$_}
-						? ($self->{prim_dict}{$_})
-						: (exists ($self->{word_dict}{$_})
-							? ($bsr, $self->{word_dict}{$_})
-							: (/(\d+)/
-								? ($self->{prim_dict}{PUSH}, 0+$1)
-								: warn "undefined word $_"
-							)
-						)
+					my $found = &{ $lkup }($_);
+
+					$found ? ($bsr, $found) : (
+						/(\d+)/
+							? ($push, 0+$1)
+							: ()
+					);
 				} @def,
 			),
-			$self->{prim_dict}{EXIT},
+			$exit,
 		);
 	}
 }
 
-<<RUNLOOP
+<<RUNLOOP;
 : LOOP
 	PARSE-WORD FIND-WORD
 	DUP IMMEDIATE? NOT ( is the word regular? )
@@ -346,3 +527,14 @@ PRELUDE
 	THEN
 ;
 RUNLOOP
+
+
+sub string_at_addr {
+	my $self = shift;
+	my $entry = shift;
+	
+	my $offset = $self->{heap}[$entry + 3];
+	my $length = $self->{heap}[$entry + 4];
+
+	my $string = join("", @{$self->{heap}}[$offset .. (($offset + $length) - 1) ]);
+}

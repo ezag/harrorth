8084491f68999e2c7f748f821494f9dfab34f3dd
Yuval Kogman <nothingmuch@woobling.org>
2005-07-30 16:34:56 +0300

    params to cmp_test.sh are like params to prove, ("t/") is the default
    darcs-hash:20050730133456-11c7b-ac23adfdba1b6ba63fb01553178e428b2f5fd067.gz

--- misc/Forth/cmp_forths.sh
+++ misc/Forth/cmp_forths.sh
@@ -4,7 +4,9 @@ for FORTH_TEST_BACKEND in gforth p5orth; do
 	echo "backend: $FORTH_TEST_BACKEND";
 
 	export FORTH_TEST_BACKEND;
-	prove -Ilib t;
+	test_files=$*;
+	if [ -z "$test_files" ]; then test_files=t; fi
+	prove -Ilib $test_files
 
 	echo
 done
